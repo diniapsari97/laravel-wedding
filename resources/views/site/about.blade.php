fb35fbc470522eb6172175f1652c35d3a1d4267f
diniapsari97 <apsaridini97@gmail.com>
2020-02-04 12:26:00 +0700

    laravel wedding

---
+++ resources/views/site/about.blade.php
@@ -0,0 +1,206 @@
+@extends('layout.master')
+@section('isihome')
+
+
+<div class="ftco-blocks-cover-1">
+  <div class="site-section-cover overlay" style="background-image: url('tampilandepan/images/pink.jpg')">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-7">
+          <h1 class="mb-3 text-primary">About Us</h1>
+          <p>Ayu Sukma Pertiwi dan Dini Apsari adalah siswa techmuda3 kami membuat project untuk mengetahui berbagai macam vendor </p>
+          <p><a href="#" class="btn btn-primary">Contact Me</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="site-section bg-dark about-me">
+ <div class="container">
+   <div class="row align-items-center">
+     <div class="col-md-6 mb-5 mb-md-0">
+       <img src="tampilandepan/images/ayu.jpg" alt="Image" class="img-fluid">
+     </div>
+     <div class="col-md-5 ml-auto">
+       <h3 class="text-white mb-5">About Me</h3>
+       <blockquote class="quote-29281">
+        <p>Ayu Sukma Pertiwi
+        </p>
+      </blockquote>
+      <p>"Bukan karena kurangnya cinta, tapi kurangnya persahabatan yang membuat pernikahan tidak bahagia".
+
+      Persahabatan tidak akan pernah berakhir hingga kamu meninggal. Maka bersahabatlah dengan pasanganmu, agar hubungan pernikahanmu lebih langgeng.</p>
+      <p>"Kenali masalahnya, ketahui risikonya, dan pelajari solusinya. Ini modal kamu untuk bisa punya pernikahan yang bahagia nanti".
+
+      Setiap tindakan pasti memiliki resiko masing-masing. Kamu harus tahu apa resikonya dan bagaimana cara menyelesaikannya, agar hubunganmu bisa dijaga.</p>
+
+
+      <div class="social_29128 white mt-5">
+        <a href="#"><span class="icon-facebook"></span></a>  
+        <a href="#"><span class="icon-instagram"></span></a>  
+        <a href="#"><span class="icon-twitter"></span></a>  
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="site-section bg-dark about-me">
+ <div class="container">
+   <div class="row align-items-center">
+     <div class="col-md-6 mb-5 mb-md-0">
+       <img src="tampilandepan/images/dini.jpg" alt="Image" class="img-fluid">
+     </div>
+     <div class="col-md-5 ml-auto">
+       <h3 class="text-white mb-5">About Me</h3>
+       <blockquote class="quote-29281">
+        <p>Dini Apsari
+        </p>
+      </blockquote>
+      <p>"Bukan karena kurangnya cinta, tapi kurangnya persahabatan yang membuat pernikahan tidak bahagia".
+
+      Persahabatan tidak akan pernah berakhir hingga kamu meninggal. Maka bersahabatlah dengan pasanganmu, agar hubungan pernikahanmu lebih langgeng.</p>
+      
+      <p>"Aku hanyalah kunang-kunang, dan kau hanyalah senja, dalam gelap kita berbagi, dalam gelap kita abadi".</p>
+
+
+      <div class="social_29128 white mt-5">
+        <a href="#"><span class="icon-facebook"></span></a>  
+        <a href="#"><span class="icon-instagram"></span></a>  
+        <a href="#"><span class="icon-twitter"></span></a>  
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+<div class="site-section bg-light">
+  <div class="container">
+
+    <div class="row mb-5 ">
+      <div class="col-md-7 text-center mx-auto">
+        <span class="subtitle-39293">Testimonials</span>
+        <h2 class="serif">What Client Says</h2>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="mb-4">
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+        </div>
+        <div>
+          <h5 class="serif mb-4">The Best WO</h5>
+          <blockquote class="quote_39823">
+            <p>Terimakasih ya Allah semua acara berjalan dengan lancar tanpa ada hambatan dan penuh dengan bahagia, terimakasih juga buat WO ini kau begitu Profesional.</p>
+          </blockquote>
+          <p>&mdash; Lydia dan Bagas</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+
+        <div class="mb-4">
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+        </div>
+        <div>
+          <h5 class="serif mb-4">The Best Dekorasi</h5>
+          <blockquote class="quote_39823">
+            <p>Terimakasih banyak untuk membantu acara pernikahan kami, dekorasi sangat memuaskan makananya juga oke. sangat komunikatif dengan klien.</p>
+          </blockquote>
+          <p>&mdash; Fika dan Andre</p>
+        </div>
+
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="mb-4">
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+          <span class="icon-star text-warning"></span>
+        </div>
+        <div>
+          <h5 class="serif mb-4">Professional Photographer</h5>
+          <blockquote class="quote_39823">
+            <p>Mengucapkan banyak terimakasih kepada WO yang telah membantu acara pernikahan kami sehingga acara berjalan dengan baik dan lancar, alhamdulilah hasilnya sungguh sangat memuaskan</p>
+          </blockquote>
+          <p>&mdash; Eka dan Andri</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="site-section bg-white">
+  <div class="container">
+    <div class="row mb-5 ">
+      <div class="col-md-7 text-center mx-auto">
+        <span class="subtitle-39293">News</span>
+        <h2 class="serif">Events</h2>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="post-entry-1 h-100">
+          <a href="single.html">
+            <img src="tampilandepan/images/img_1.jpg" alt="Image"
+            class="img-fluid">
+          </a>
+          <div class="post-entry-1-contents">
+
+            <h2><a href="single.html"> </a></h2>
+            <span class="meta d-inline-block mb-3">Desember 30, 2019 <span class="mx-2">by</span> <a href="#">Admin</a></span>
+            <p> </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="post-entry-1 h-100">
+          <a href="single.html">
+            <img src="tampilandepan/images/img_2.jpg" alt="Image"
+            class="img-fluid">
+          </a>
+          <div class="post-entry-1-contents">
+
+            <h2><a href="single.html"> </a></h2>
+            <span class="meta d-inline-block mb-3">Desember 30, 2019 <span class="mx-2">by</span> <a href="#">Admin</a></span>
+            <p> </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="post-entry-1 h-100">
+          <a href="single.html">
+            <img src="tampilandepan/images/img_3.jpg" alt="Image"
+            class="img-fluid">
+          </a>
+          <div class="post-entry-1-contents">
+
+            <h2><a href="single.html"> </a></h2>
+            <span class="meta d-inline-block mb-3">Desember 30, 2019 <span class="mx-2">by</span> <a href="#">Admin</a></span>
+            <p> </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,266 @@
+@extends('layout.master')
+@section('isihome')
+
+        <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" style="background-image: url('tampilandepan/images/girl.jpg')">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-7">
+              
+              <h1 class="mb-3 text-primary">Wedding &amp; Organizer</h1>
+              <p>Sesuai dengan namanya maka WO (Wedding Organizer) adalah jasa yang pengorganisasian segala aktivitas persiapan pernikahan yang berkaitan dengan kebutuhan dalam suatu pesta pernikahan..</p>
+              <p><a href="#" class="btn btn-primary">Contact Me</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<div class="site-section">
+      <div class="container">
+        <div class="row mb-5 ">
+          <div class="col-md-7 text-center mx-auto">
+            <span class="subtitle-39293">My Works</span>
+            <h2 class="serif">See My Works</h2>
+          </div>
+        </div>
+       
+        <div id="posts" class="row no-gutter">
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_1.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_1.jpg">
+            </a>
+          </div>
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_2.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_2.jpg">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_3.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_3.jpg">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_4.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_4.jpg">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_5.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_5.jpg">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_6.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_6.jpg">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_1.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_1.jpg">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_2.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_2.jpg">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_3.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_3.jpg">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_4.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_4.jpg">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_5.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_5.jpg">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="tampilandepan/images/img_6.jpg" class="item-wrap" data-fancybox="gal">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="tampilandepan/images/img_6.jpg">
+            </a>
+          </div>
+
+
+        </div>
+
+      </div>
+
+
+    
+
+   
+  
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-5 ">
+          <div class="col-md-7 text-center mx-auto">
+            <span class="subtitle-39293">See The Video</span>
+            <h2 class="serif">See The Video</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+
+            <a href="https://vimeo.com/191947042" data-fancybox  class="btn-video_38929">
+              <span><span class="icon-play"></span></span>
+              <img src="tampilandepan/images/img_1.jpg" alt="Image" class="img-fluid">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-light">
+      <div class="container">
+
+        <div class="row mb-5 ">
+          <div class="col-md-7 text-center mx-auto">
+            <span class="subtitle-39293">Testimonials</span>
+            <h2 class="serif">What Client Says</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <div class="mb-4">
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+            </div>
+            <div>
+              <h5 class="serif mb-4">The Best WO</h5>
+              <blockquote class="quote_39823">
+                <p>Terimakasih ya Allah semua acara berjalan dengan lancar tanpa ada hambatan dan penuh dengan bahagia, terimakasih juga buat WO ini kau begitu Profesional.</p>
+              </blockquote>
+              <p>&mdash; Lydia dan Bagas</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            
+            <div class="mb-4">
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+            </div>
+            <div>
+              <h5 class="serif mb-4">The Best Dekorasi</h5>
+              <blockquote class="quote_39823">
+                <p>Terimakasih banyak untuk membantu acara pernikahan kami, dekorasi sangat memuaskan makananya juga oke. sangat komunikatif dengan klien .</p>
+              </blockquote>
+              <p>&mdash; Fika dan Andre</p>
+            </div>
+
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="mb-4">
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+              <span class="icon-star text-warning"></span>
+            </div>
+            <div>
+              <h5 class="serif mb-4">Professional Photographer</h5>
+              <blockquote class="quote_39823">
+                <p>Mengucapkan banyak terimakasih kepada WO yang telah membantu acara pernikahan kami sehingga acara berjalan dengan baik dan lancar, alhamdulilah hasilnya sungguh sangat memuaskan</p>
+              </blockquote>
+              <p>&mdash; Eka dan Andri</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-white">
+      <div class="container">
+        <div class="row mb-5 ">
+          <div class="col-md-7 text-center mx-auto">
+            <span class="subtitle-39293">News</span>
+            <h2 class="serif">Events</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="post-entry-1 h-100">
+              <a href="single.html">
+                <img src="tampilandepan/images/img_1.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                
+                <h2><a href="single.html"> </a></h2>
+                <span class="meta d-inline-block mb-3">Desember 30, 2019 <span class="mx-2">by</span> <a href="#">Admin</a></span>
+                <p> </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="post-entry-1 h-100">
+              <a href="single.html">
+                <img src="tampilandepan/images/img_2.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                
+                <h2><a href="single.html"> </a></h2>
+                <span class="meta d-inline-block mb-3">Desember 30, 2019 <span class="mx-2">by</span> <a href="#">Admin</a></span>
+                <p> </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="post-entry-1 h-100">
+              <a href="single.html">
+                <img src="tampilandepan/images/img_3.jpg" alt="Image"
+                 class="img-fluid">
+              </a>
+              <div class="post-entry-1-contents">
+                
+                <h2><a href="single.html"> </a></h2>
+                <span class="meta d-inline-block mb-3">Desember 30, 2019 <span class="mx-2">by</span> <a href="#">Admin</a></span>
+                <p> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+     @endsection
